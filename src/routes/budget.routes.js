cf58f804342492a7ccdf573f48b12318127aee26
usamamunim <usama.munim48@gmail.com>
2023-04-10 15:13:20 +0500

    Added validate budget middleware

--- src/routes/budget.routes.js
+++ src/routes/budget.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = express.Router();
 const Budget = require('../models/Budget');
 const validateToken = require('../middlewares/authToken');
-const { check, validationResult } = require('expressValidator/check');
+const { validateBudget } = require('../middlewares/validator');
 const {
   getAllBudgets,
   getMyBudgets,
@@ -13,6 +13,7 @@ const {
   updateBudget,
   deleteBudget,
 } = require('../controllers/budget.controller');
+
 routes.get('/', validateToken, getAllBudgets);
 
 routes.get('/my', validateToken, getMyBudgets);
@@ -23,20 +24,8 @@ routes.get('/:user_id', validateToken, getBudgetByUserId);
 
 routes.get('/category/:category_id', validateToken, getBudgetByCategoryId);
 
-routes.post(
-  '/',
-  check('threshold', 'Threshold is required').notEmpty(),
-  check('category_id', 'CategoryId is required').notEmpty(),
-  validateToken,
-  createBudget
-);
+routes.post('/', validateToken, validateBudget, createBudget);
 
-routes.update(
-  '/:id',
-  check('threshold', 'Threshold is required').notEmpty(),
-  check('category_id', 'CategoryId is required').notEmpty(),
-  validateToken,
-  updateBudget
-);
+routes.update('/:id', validateToken, validateBudget, updateBudget);
 
 routes.delete('/:id', validateToken, deleteBudget);
